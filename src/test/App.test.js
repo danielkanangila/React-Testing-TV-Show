b104309b581929f28f408dcb880306f598c707b3
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-03-12 00:12:41 -0400

    writed test for Episodes component and passed

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,11 +3,13 @@ import { render, fireEvent, waitForElement, wait } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect';
 import { fetchShow as mockFetchShow } from '../api/fetchShow';
 import App from '../App';
+import Episodes from '../components/Episodes';
 import data from './dataSample';
+import { formatSeasons } from '../utils/formatSeasons';
 
 jest.mock('../api/fetchShow');
 
-test("Fetching data and renders app without crashing!", async () => {
+test("Fetching initial data and app renders without crashing!", async () => {
     mockFetchShow.mockResolvedValueOnce({data})
     const {getByTestId} = render(<App />);
     expect(getByTestId(/fetching-loader/i)).toHaveTextContent("Fetching data...");
@@ -15,3 +17,9 @@ test("Fetching data and renders app without crashing!", async () => {
     expect(mockFetchShow).toHaveBeenCalledTimes(1);
     expect(getByTestId(/resolved-name/i)).toHaveTextContent(data.name);
 });
+
+test("Renders Episodes components without crashing", async () => {
+    const { getAllByTestId } = render(<Episodes episodes={data._embedded.episodes} />)
+
+    expect(getAllByTestId(/resolved-episode/i)).toHaveLength(2);
+})
--- src/test/dataSample.js
+++ src/test/dataSample.js
@@ -27,6 +27,27 @@ export default {
                                 href: "http://api.tvmaze.com/episodes/553946"
                             }
                         }
+                    },
+                    {
+                        id: 578663,
+                        url: "http://www.tvmaze.com/episodes/578663/stranger-things-1x02-chapter-two-the-weirdo-on-maple-street",
+                        name: "Chapter Two: The Weirdo on Maple Street",
+                        season: 1,
+                        number: 2,
+                        airdate: "2016-07-15",
+                        airtime: "",
+                        airstamp: "2016-07-15T12:00:00+00:00",
+                        runtime: 60,
+                        image: {
+                        medium: "http://static.tvmaze.com/uploads/images/medium_landscape/72/181376.jpg",
+                        original: "http://static.tvmaze.com/uploads/images/original_untouched/72/181376.jpg"
+                        },
+                        summary: "<p>While the search for the missing Will continues, Joyce tells Jim about a call she apparently received from her son. Meanwhile, Jane warns Mike that there are bad people after her, and he realizes that she knows what happened to Will.</p>",
+                        _links: {
+                        self: {
+                                href: "http://api.tvmaze.com/episodes/578663"
+                            }
+                        }
                     }
                 ]
             }
